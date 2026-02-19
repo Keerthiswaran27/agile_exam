40c68eb36fc5706af0237527e4907e29816af3fa
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-02-19 08:30:52 +0530

    initial commit of app.py code

---
+++ app.py
@@ -0,0 +1,8 @@
+import sys
+
+str1 = sys.argv[1]
+
+print("=================================")
+print("String Concatenation Result")
+print("=================================")
+print("parameter String :", str1)
